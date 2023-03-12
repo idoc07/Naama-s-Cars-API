41e80adb626d7f313aba400ee631f5c1a29331ff
idoc07 <108226408+idoc07@users.noreply.github.com>
2023-03-12 19:52:46 +0200

    0.0.1

--- microservice/run_uvicorn.py
+++ microservice/run_uvicorn.py
@@ -1,6 +1,6 @@
 import uvicorn
 import app_config
 
-# Auto run uvicorn
+# Auto run uvicorn 2
 if __name__ == "__main__":
     uvicorn.run(app_config.APP_NODE, host=app_config.APP_HOST, port=app_config.APP_PORT)
